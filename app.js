a2c8cd2404d8f035d7e4f7719630a89da5a97769
Daniel Zych <danjzych@gmail.com>
2023-08-15 18:42:02 -0500

    Got to point of getting JSON response, sorting out appending gif to page

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+'use strict';
+
 console.log("Let's get this party started!");
+
+const $form = $('form');
+
+//when form is submitted, we want to get the value of our input at that moment, and use that as a search term on giphy
+//we will then add a gif to the page after getting a response from giphy
+
+async function getMeme(evt) {
+  //get the current input value and store in a variable "searchterm"
+  //turn that searchterm into a parameter URLSearchParams();
+  //get result using await fetch(APIENDPOINT) store in a variable called response
+  //from response we get our result .text()/.json(); and grab the first gif in the response
+  //put the gif in meme container
+  //outside of this function, we will probably wanna have some basic styling for the meme
+
+  evt.preventDefault();
+
+  const gifSearchTerm = $('#gifSearchTerm').val();
+
+  const params = new URLSearchParams({q: gifSearchTerm});
+  const api_key = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+
+  const response = await fetch(`http://api.giphy.com/v1/gifs/search?${params}&api_key=${api_key}`);
+  const responseJSON = await response.json(); //why can't we add '.data[0].url' to the end of this?
+  const gifURL = responseJSON.data[0].url;
+
+  $('.gifContainer').append($(`<img src='${gifURL}' />`));
+
+
+}
+
+$form.on('submit', getMeme);
